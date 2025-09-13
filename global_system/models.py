31814ce8193733d8d6f49ec01aa2b0425e38d6b8
b56642649 <b56642649@gmail.com>
2025-09-13 15:20:59 +0300

    second commit11111

--- global_system/models.py
+++ global_system/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
-    desciption = models.TextField()
+    description = models.TextField(blank=True)
     food_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
--- global_system/views.py
+++ global_system/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from.models import Food
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
@@ -8,8 +9,20 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username =form.cleaned_data.get("username")
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт {username} создан!')
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'global_system/reqister.html', {"form": form})
+    return render(request, 'global_system/register.html', {'form': form})
+
+def food_list(request):
+    search_query = request.GET.get('search', '')
+    foods = Food.objects.all()
+
+    if search_query:
+        foods = foods.filter(name__icontains=search_query)
+
+    return render(request, 'global_system/food_list.html',{
+        'foods': foods
+    })
