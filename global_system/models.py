470db4e284467277861aa12cbdcec928236e21ea
b56642649 <b56642649@gmail.com>
2025-09-13 11:57:46 +0300

    fist commit11111

--- global_system/models.py
+++ global_system/models.py
@@ -3,3 +3,14 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=100)
     desciption = models.TextField()
+    food_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Table(models.Model):
+    table_number = models.CharField(max_length=10)
+    capacity = models.PositiveIntegerField()
+    table_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_avaliable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.table_number}'
+
